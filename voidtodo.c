bb1b8768d31fee985a8a55e8d35fd25c45aa96d3
Lai0602 <83352569+lai0602-real@users.noreply.github.com>
2026-08-31 21:29:16 +0800

    Format code

--- voidtodo.c
+++ voidtodo.c
@@ -3,60 +3,60 @@
 #include <stdbool.h>
 
 int main(){
-  
-  // create file for storing tasks
+    printf("=======================\n"
+        "WELCOME TO VOID's TO-DO LIST (Pre-alpha version)\n"
+        "=======================\n");
 
-  printf("=======================\nWELCOME TO VOID's TO-DO LIST (Pre-alpha version)\n=======================\n");
-  FILE *tasks = fopen("myTasks.txt", "a");
-  char task[1024];
-  bool newTask = true;
-  char response[5];
-  char response2[5];
-  char buffer[1024];
+    // create file for storing tasks
 
-  if(tasks == NULL){
-    printf("Error opening the file!\n");
-    return 1;
-  }
-  while(newTask){
-    
-    printf("\nEnter a task: ");
-    fgets(task, sizeof(task), stdin);
-    task[strcspn(task, "\n")] = '\0';
-    
+    FILE *tasks = fopen("myTasks.txt", "a");
+    char task[1024];
+    bool newTask = true;
+    char response[5];
+    char response2[5];
+    char buffer[1024];
 
-    printf("Do you want add new task? (yes or no):");
-    fgets(response, sizeof(response), stdin);
-    response[strcspn(response, "\n")] = '\0';
-   
-    fprintf(tasks, "%s\n", task); 
-    if (strcmp(response, "yes") != 0){
-      newTask= false;
+    if(tasks == NULL) {
+        printf("Error opening the file!\n");
+        return 1;
     }
-  
-  }
-  fclose(tasks);
 
-  tasks = fopen("myTasks.txt", "r");
-  if(tasks == NULL){
+    while(newTask) {
+        printf("\nEnter a task: ");
+        fgets(task, sizeof(task), stdin);
+        task[strcspn(task, "\n")] = '\0';
+
+        printf("Do you want add new task? (yes or no):");
+        fgets(response, sizeof(response), stdin);
+        response[strcspn(response, "\n")] = '\0';
+
+        fprintf(tasks, "%s\n", task); 
+        if (strcmp(response, "yes") != 0) {
+          newTask= false;
+        }
+    }
+
+    fclose(tasks);
+
+    tasks = fopen("myTasks.txt", "r");
+    if(tasks == NULL) {
       printf("Error open the file!\n");
       return 1;
-  }
-
-  printf("Do you want to see your tasks? (yes or no): ");
-  fgets(response2, sizeof(response2), stdin);
-  response2[strcspn(response2, "\n")] = '\0';
-  
-  if(strcmp(response2, "yes") == 0){
-    while(fgets(buffer, sizeof(buffer), tasks) != NULL){
-      printf("%s", buffer);
     }
-  }
-  else{
-    printf("Exiting the app!\n");
-  }
 
-  fclose(tasks);
-  
-  return 0;
+    printf("Do you want to see your tasks? (yes or no): ");
+    fgets(response2, sizeof(response2), stdin);
+    response2[strcspn(response2, "\n")] = '\0';
+
+    if(strcmp(response2, "yes") == 0) {
+        while(fgets(buffer, sizeof(buffer), tasks) != NULL){
+          printf("%s", buffer);
+        }
+    } else {
+        printf("Exiting the app!\n");
+    }
+
+    fclose(tasks);
+
+    return 0;
 }
